a504b28a373e5938baaf9b54131e2f52f4d92dda
scseong <goidtv@naver.com>
2023-11-21 23:04:04 +0900

    feat: 라우팅 설정 (#7)

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
 import Layout from 'components/Layout/Layout';
+import { AllBoard, BoardDetail, Home, MyPage, NewBoard } from 'pages';
 
 export default function Router() {
   return (
@@ -9,6 +9,10 @@ export default function Router() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/boards" element={<AllBoard />} />
+          <Route path="/boards/:id" element={<BoardDetail />} />
+          <Route path="/boards/new" element={<NewBoard />} />
+          <Route path="/mypage" element={<MyPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/AllBoard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AllBoard() {
+  return <div>AllBoard</div>;
+}
