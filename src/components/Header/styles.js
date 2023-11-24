c66728a91953042589636f5ac44f84c5124aa149
HIITMEMARIO <alswnsgur@nate.com>
2023-11-25 02:15:27 +0900

    feat: 마이페이지 모달UI, 모달 토글기능 (#36)

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,25 +4,24 @@ export const Stcontainer = styled.div`
   width: 100%;
   display: flex;
   height: 80px;
-  /* background-color: red; */
   align-items: center;
-  /* border-bottom: 1px solid #bebebe;
-  box-shadow: 0px 1px 10px black; */
   padding-top: 30px;
   justify-content: space-around;
   @media screen and (max-width: 1200px) {
     height: 500px;
     flex-direction: column;
-    /* background-color: red; */
     align-items: center;
   }
 `;
-
+export const StLogoBox = styled.div`
+  display: flex;
+  position: relative;
+`;
 export const StLoginBtn = styled.button`
+  width: 100px;
   display: flex;
   height: 80px;
-  width: 10%;
-  /* background-color: green; */
+  /* width: 10%; */
   align-items: center;
   justify-content: center;
   cursor: pointer;
@@ -35,20 +34,18 @@ export const StLoginBtn = styled.button`
 `;
 
 export const StLogo = styled.img`
-  width: 20rem;
-  height: auto;
+  /* width: 20rem; */
+  height: 80px;
   display: flex;
-  /* padding-left: 1px; */
-  /* background-color: rebeccapurple; */
+
   cursor: pointer;
   @media screen and (max-width: 1200px) {
-    height: 10rem;
+    height: 80px;
     flex-direction: column;
   }
 `;
 
 export const StNav = styled.div`
-  /* background-color: #bebebe; */
   width: 50%;
   height: 80px;
   display: flex;
@@ -64,7 +61,6 @@ export const StNav = styled.div`
   @media screen and (max-width: 1200px) {
     height: 300px;
     flex-direction: column;
-    /* background-color: red; */
     align-items: center;
     border-bottom: 1px solid black;
   }
---
+++ src/components/Headermodal/style.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+export const StModalContainer = styled.div`
+  position: absolute;
+  width: 400%;
+  z-index: 10;
+  top: 110%;
+  right: 25%;
+  border-radius: 10px;
+  border: 1px solid #e4ebf0;
+`;
+
+export const StArrowBox = styled.div`
+  background-color: #fff;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  right: 10px;
+  top: -10px;
+  bottom: 0;
+  transform: rotate(45deg);
+  transform-origin: 0 0;
+  z-index: -1;
+  border: 1px solid #e4ebf0;
+`;
+
+export const StUserInfo = styled.div`
+  height: 150px;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  border-radius: 10px 10px 0 0;
+
+  img {
+    margin-top: 30px;
+    align-self: flex-start;
+    background-color: red;
+    width: 20%;
+    height: 50%;
+    border-radius: 50%;
+    margin-left: 4%;
+  }
+`;
+
+export const StNameEmailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    margin-left: 10px;
+    display: flex;
+    width: 280px;
+    font-size: 26px;
+    font-weight: 800;
+    padding: 30px;
+    word-break: break-all;
+    /* background-color: ; */
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  p {
+    width: 200px;
+    margin-left: 40px;
+    margin-bottom: 40px;
+    display: flex;
+    background-color: #fff;
+    height: 30%;
+    align-items: center;
+  }
+`;
+
+export const StMypage = styled.div`
+  display: flex;
+  height: 60px;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  font-weight: 800;
+  border-bottom: 1px solid #e4ebf0;
+  border-top: 1px solid #e4ebf0;
+`;
+
+export const StLogout = styled.div`
+  display: flex;
+  height: 60px;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  color: #e8344e;
+  border-radius: 0 0 10px 10px;
+  cursor: pointer;
+`;
