728afa9e3694946b853d24531e63f32252377187
scseong <goidtv@naver.com>
2023-11-27 05:44:20 +0900

    feat: 글쓰기 에디터 사진 firebase 업로드 (#62)

--- src/components/BoardEditor/BoardEditor.jsx
+++ src/components/BoardEditor/BoardEditor.jsx
@@ -4,20 +4,26 @@ import React, {
   useImperativeHandle,
   useRef,
 } from 'react';
+import { storage } from 'config/firebase';
+import { uuidv4 } from '@firebase/util';
+import {
+  ref as firebaseRef,
+  getDownloadURL,
+  uploadBytes,
+} from '@firebase/storage';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 function BoardEditor({ initialValue, height = '400px' }, ref) {
+  const uploadURL = `image/${uuidv4()}`;
   const editorRef = useRef();
+
   useImperativeHandle(ref, () => ({
     set: () => editorRef.current?.getInstance().getHTML(),
   }));
 
   useEffect(() => {
-    if (!editorRef.current) return;
-
     if (editorRef.current) {
-      console.log(editorRef);
       editorRef.current?.getInstance().focus();
       if (initialValue === '') {
         editorRef.current?.getInstance().reset();
@@ -37,6 +43,20 @@ function BoardEditor({ initialValue, height = '400px' }, ref) {
         useCommandShortcut={true}
         usageStatistics={false}
         hideModeSwitch={true}
+        hooks={{
+          addImageBlobHook: (blob, callback) => {
+            const imgs = firebaseRef(storage, uploadURL);
+            uploadBytes(imgs, blob).then((data) => {
+              getDownloadURL(data.ref)
+                .then((imageUrl) => {
+                  callback(imageUrl);
+                })
+                .catch((error) => {
+                  alert('사진 업로드에 실패했습니다.');
+                });
+            });
+          },
+        }}
       />
     </>
   );
